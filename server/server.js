2bd8d8eaddefab8491869214cc14cbf1d8e6dbab
nananso <uchennansoha2017@yahoo.com>
2018-07-23 15:55:52 -0700

    second_commit

---
+++ server/server.js
@@ -0,0 +1,14 @@
+
+
+const express = require('express');
+const path = require('path');
+const app = express();
+
+const publicPath = path.join(__dirname, '../' );
+const port = process.env.PORT || 3000;
+
+app.use(express.static(publicPath));
+
+app.listen(port, () =>{
+  console.log(`server is up on port  ${port}`);
+})
